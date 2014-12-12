7f6a27dc5cc63c1514fea9eb38e1c152adf93e46
Nisansa Dilushan de Silva <nhnisansa@gmail.com>
2014-12-12 01:05:51 -0800

    Max stat collection

--- Checkers/Program.cs
+++ Checkers/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System;
+using System.IO;
 
 namespace Checkers
 {
@@ -50,92 +52,110 @@ namespace Checkers
             DateTime t1 = DateTime.Now;
            // int i = 0;
             List<Int32> powers = new List<Int32>();
+            List<Int32> timeLimits = new List<Int32>();
             for (int i = 0; i <= 10; i++)
             {
                 powers.Add(i);
+                timeLimits.Add((i+1) * 10);
             }
-            for (int i = 20; i <= 100; i=i+10)
+      /*     for (int i = 20; i <= 100; i=i+10)
             {
                 powers.Add(i);
-            }
-            double[] stats = new double[powers.Count];
+      //          timeLimits.Add(i * 10);
+            }  */ 
 
-            gameCount = powers.Count;
-            int loopCount = 10;
+            double[,] stats = new double[powers.Count,timeLimits.Count];
 
-            for (int k = 0; k < loopCount; k++)
+            for (int l = 0; l < timeLimits.Count; l++)
             {
-
-
                 for (int i = 0; i < powers.Count; i++)
                 {
-
-
-                    //  for (; i < gameCount; i++)
-                    //  {
-                    Console.WriteLine("Playing game " + ((i + 1) + (k * powers.Count)) + " of " + (loopCount * powers.Count));
-                    CheckersGame.Reset(depth);
-                    // CheckersGame.setSpecial(true);
-                    CheckersGame.Power = powers[i];
-                    Console.WriteLine("Power " + powers[i]);
-                    CheckersGame game = (CheckersGame)CheckersGame.getInstance();
-                    game.UseNewFunction = new bool[] { false, false };
-                    game.UseNewFunction = new bool[] { true, true };
-                    int steps = 0;
-                    int depthSum = 0;
-                    while (!game.Current.IsTerminalNode())
-                    {
-                        DateTime t3 = DateTime.Now;
-                        CheckersBoard best = game.ComputerMakeMove(0);
-                        for (int j = 1; j <= depth; j++)
-                        {
-                            DateTime t4 = DateTime.Now;
-                            if ((t4 - t3).TotalMilliseconds < 100)
-                            {
-                                best = game.ComputerMakeMove(j);
-                                game.updateState(best);
-                                if (j == depth)
-                                {
-                                    depthSum += j;
-                                }
-                            }
-                            else
-                            {
-                                depthSum += (j - 1);
-                                break;
-                            }
-                        }
-                        steps++;
-                    }
-                    stats[i] += ((double)depthSum / (double)steps);
-                    Console.WriteLine("Avg depth: " + stats[i]);
-
-                    /*              if (game.Current.RecursiveScore > 0)
-                                  {
-                                      wVictories++;
-                                  }
-                                  else if (game.Current.RecursiveScore < 0)
-                                  {
-                                      bVictories++;
-                                  }
-                                  totalSteps += steps;
-
-                                  double brancFac=game.GetBranchingFactor();
-                                  for (int j = 0; j < CheckersGame.PruneHeights.Count; j++)
-                                  {
-                                      totalpruned += (int)Math.Pow(brancFac, CheckersGame.PruneHeights[j]);
-                                  }
-                  */
-                    //  totalpruned += CheckersGame.PrunedCount;                
-                    // }
+                    stats[i, l] = Double.NegativeInfinity;
                 }
             }
 
 
-            for (int i = 0; i < powers.Count; i++)
+            gameCount = powers.Count;
+            int loopCount = 10;
+
+
+            for (int l = 0; l < timeLimits.Count; l++)
+            {
+                for (int k = 0; k < loopCount; k++)
+                {
+
+
+                    for (int i = 0; i < powers.Count; i++)
+                    {
+
+
+                        //  for (; i < gameCount; i++)
+                        //  {
+                        Console.WriteLine("Time Limit : " + timeLimits[l]);
+                        Console.WriteLine("Playing game " + ((i + 1) + (k * powers.Count)) + " of " + (loopCount * powers.Count));
+                        CheckersGame.Reset(depth);
+                        // CheckersGame.setSpecial(true);
+                        CheckersGame.Power = powers[i];
+                        Console.WriteLine("Power " + powers[i]);
+                        CheckersGame game = (CheckersGame)CheckersGame.getInstance();
+                        game.UseNewFunction = new bool[] { false, false };
+                        game.UseNewFunction = new bool[] { true, true };
+                        int steps = 0;
+                        int depthSum = 0;
+                        while (!game.Current.IsTerminalNode())
+                        {
+                            DateTime t3 = DateTime.Now;
+                            CheckersBoard best = game.ComputerMakeMove(0);
+                            for (int j = 1; j <= depth; j++)
+                            {
+                                DateTime t4 = DateTime.Now;
+                                if ((t4 - t3).TotalMilliseconds < timeLimits[l])
+                                {
+                                    best = game.ComputerMakeMove(j);
+                                    game.updateState(best);
+                                    if (j == depth)
+                                    {
+                                        depthSum += j;
+                                    }
+                                }
+                                else
+                                {
+                                    depthSum += (j - 1);
+                                    break;
+                                }
+                            }
+                            steps++;
+                        }
+                       // stats[i,l] += ((double)depthSum / (double)steps);
+                        stats[i, l] = Math.Max(((double)depthSum / (double)steps),stats[i, l]);
+                        Console.WriteLine("Avg depth: " + stats[i,l]);
+
+                        /*              if (game.Current.RecursiveScore > 0)
+                                      {
+                                          wVictories++;
+                                      }
+                                      else if (game.Current.RecursiveScore < 0)
+                                      {
+                                          bVictories++;
+                                      }
+                                      totalSteps += steps;
+
+                                      double brancFac=game.GetBranchingFactor();
+                                      for (int j = 0; j < CheckersGame.PruneHeights.Count; j++)
+                                      {
+                                          totalpruned += (int)Math.Pow(brancFac, CheckersGame.PruneHeights[j]);
+                                      }
+                      */
+                        //  totalpruned += CheckersGame.PrunedCount;                
+                        // }
+                    }
+                }
+            }
+
+      /*      for (int i = 0; i < powers.Count; i++)
             {
                 Console.WriteLine(powers[i] + " -> " + (stats[i] / loopCount));
-            }
+            }*/
 
 
             DateTime t2 = DateTime.Now;
@@ -145,6 +165,33 @@ namespace Checkers
     //        Console.WriteLine("Black won count: " + bVictories);
     //        Console.WriteLine("Time Diff: " + (t2 - t1).TotalMilliseconds);
     //        Console.WriteLine("Pruning cuts : " + totalpruned);
+
+
+            string path = @"D:\Documents\Visual Studio 2008\Projects\Solver\" + powers[0] + "_" + powers[powers.Count-1] + ".csv";
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                String line = " ,";
+                for (int l = 0; l < timeLimits.Count; l++)
+                {
+                    line += timeLimits[l]+",";
+                }
+                sw.WriteLine(line);
+
+                for (int i = 0; i < powers.Count; i++)
+                {
+                    line = powers[i]+",";
+                    for (int l = 0; l < timeLimits.Count; l++)
+                    {
+                       // line += (stats[i,l] / loopCount) + ",";
+                        line += stats[i, l] + ",";
+                    }
+                    sw.WriteLine(line);                   
+                }
+
+             }
+
+            Console.WriteLine("Done!");
+
             Console.ReadLine();
             
             
